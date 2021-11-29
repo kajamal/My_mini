e97ec077abf8f0e66d5800f745070358481c2f55
kajamal <kajasharif9894@gmail.com>
2021-11-29 14:14:28 +0530

    mini project is completed

---
+++ src/com/mini_project/Mini_project.java
@@ -0,0 +1,12 @@
+package com.mini_project;
+
+public class Mini_project {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","H:\\eclipse workspace\\Testing_Selenium\\Driver\\chromedriver.exe");
+		
+		webelement driver = new chromedriver();
+	}
+
+}
